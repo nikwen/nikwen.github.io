90843dee4beee0df8b67f9faa612414c0c860840
Niklas Wenzel <nikwen.developer@gmail.com>
2016-02-06 00:57:12 +0100

    Fix issue with code overflow and nav menu in rebuilding click post

--- js/main.js
+++ js/main.js
@@ -5,27 +5,31 @@ var icon = normal !== null ? normal : reverse;
 
 // Toggle the "menu-open" % "menu-opn-left" classes
 function toggle() {
-	  var navRight = document.getElementById("nav");
-	  var navLeft = document.getElementById("nav-left");
-	  var nav = navRight !== null ? navRight : navLeft;
+  var navRight = document.getElementById("nav");
+  var navLeft = document.getElementById("nav-left");
+  var nav = navRight !== null ? navRight : navLeft;
 
-	  var button = document.getElementById("menu");
-	  var site = document.getElementById("wrap");
-	  
-	  if (nav.className == "menu-open" || nav.className == "menu-open-left") {
-	  	  nav.className = "";
-	  	  button.className = "";
-	  	  site.className = "";
-	  } else if (reverse !== null) {
-	  	  nav.className += "menu-open-left";
-	  	  button.className += "btn-close";
-	  	  site.className += "fixed";
-	  } else {
-	  	  nav.className += "menu-open";
-	  	  button.className += "btn-close";
-	  	  site.className += "fixed";
-	    }
-	}
+  var navMenu = document.getElementById("nav-menu");
+  var button = document.getElementById("menu");
+  var site = document.getElementById("wrap");
+
+  if (nav.className == "menu-open" || nav.className == "menu-open-left") {
+	  nav.className = "";
+	  button.className = "";
+	  navMenu.className = "";
+	  site.className = "";
+  } else if (reverse !== null) {
+	  nav.className += "menu-open-left";
+	  button.className += "btn-close";
+	  navMenu.className += "fixed";
+	  site.className += "static";
+  } else {
+	  nav.className += "menu-open";
+	  button.className += "btn-close";
+	  navMenu.className += "fixed";
+	  site.className += "static";
+  }
+}
 
 // Ensures backward compatibility with IE old versions
 function menuClick() {
